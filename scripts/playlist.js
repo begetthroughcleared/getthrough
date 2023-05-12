937dfc98208ab5719f6d156e1c8d191d516d452b
begetthroughcleared <132090477+begetthroughcleared@users.noreply.github.com>
2023-05-12 15:52:21 +0530

    added folders
    scripts, preview, and styles

---
+++ scripts/playlist.js
@@ -0,0 +1,322 @@
+const commonWebsitePlaylist = ["https://www.javatpoint.com/electrical-machines-tutorial",
+    "#",
+    "#",
+    "#",
+    "https://www.javatpoint.com/physics",
+    "#",
+    "https://www.javatpoint.com/english-grammar",
+    "https://www.javatpoint.com/chemistry",
+    "https://www.javatpoint.com/c-programming-language-tutorial",
+    "https://www.javatpoint.com/data-structure-tutorial",
+    "#",
+    "#",
+    "https://www.javatpoint.com/computer-organization-and-architecture-tutorial",
+    "#",
+    "#",
+    "https://www.javatpoint.com/digital-electronics",
+    "https://www.javatpoint.com/discrete-mathematics-tutorial",
+    "https://www.javatpoint.com/daa-tutorial",
+    "https://www.javatpoint.com/operating-system",
+    "https://www.javatpoint.com/java-tutorial",
+    "https://www.javatpoint.com/dbms-tutorial",
+    "https://www.javatpoint.com/automata-tutorial",
+    "https://www.w3schools.com/html/default.asp",
+    "https://www.javatpoint.com/net-framework",
+    "https://www.tutorialspoint.com/signals_and_systems/index.htm",
+    "https://www.tutorialspoint.com/managerial_economics/index.html",
+    "https://www.geeksforgeeks.org/introduction-of-compiler-design",
+    "https://www.geeksforgeeks.org/basics-computer-networking",
+    "https://www.javatpoint.com/software-engineering",
+    "https://www.javatpoint.com/artificial-intelligence-ai",
+    "https://www.geeksforgeeks.org/android-tutorial",
+    "https://www.javatpoint.com/microprocessor-tutorial",
+    "https://www.tutorialspoint.com/cryptography/index.htm",
+    "https://www.javatpoint.com/machine-learning",
+    "https://www.w3schools.com/python",
+    "https://www.javatpoint.com/uml",
+    "https://www.javatpoint.com/cloud-computing",
+    "https://www.geeksforgeeks.org/pattern-recognition-introduction/",
+    "#",
+    "https://www.geeksforgeeks.org/what-is-data-science/",
+    "https://www.javatpoint.com/iot-internet-of-things",
+    "#"
+];
+
+const englishVideoPlaylist = ["https://www.youtube.com/watch?v=IZA_bJiGiJc&list=PL_mruqjnuVd8LP2z0c4yBwKAGEiEW_Si9",
+    "#",
+    "https://www.youtube.com/watch?v=Z4WN2Z9okfs&list=PLIhUrsYr8yHxfOmcGIeUThHEr9lQ3IXRS",
+    "https://www.youtube.com/watch?v=WG6H2pISUzQ&list=PLIhUrsYr8yHwDUrVYmUNYkEeZgZTvoIfS",
+    "https://www.youtube.com/watch?v=MHL3xn3m3ak&list=PLm_MSClsnwm-K6dR5w43Qcj8EzOW823A1",
+    "https://www.youtube.com/watch?v=gDzYDn9QCoU&list=PLm_MSClsnwm-PxgKwf3IRErhJdS89VP5X",
+    "https://www.youtube.com/watch?v=xVFMmyROOo4&list=PLzf4HHlsQFwIQUeZq_ykEVB6qZrTRnJZn&index=1",
+    "https://www.youtube.com/watch?v=3O6OfCaVadI&list=PLm_MSClsnwm9p_yaZ8zIW1LxkK7_n98gD",
+    "https://www.youtube.com/watch?v=6VT8hDr2GhU&list=PLfVsf4Bjg79CZ5kHTiQHcm-l2q8j06ofd",
+    "https://www.youtube.com/watch?v=AT14lCXuMKI&list=PLdo5W4Nhv31bbKJzrsKfMpo_grxuLl8LU",
+    "https://www.youtube.com/watch?v=7jaa1rlW7Ak&list=PLBlnK6fEyqRiw-GZRqfnlVIBz9dxrqHJS",
+    "https://www.youtube.com/watch?v=LKADevceuSo&list=PLG_pOUNcDW47LfvBVo0HEdUV3MnwKCnS6",
+    "https://www.youtube.com/watch?v=Ol8D69VKX2k&list=PLBlnK6fEyqRgLLlzdgiTUKULKJPYc0A4q",
+    "#",
+    "https://www.youtube.com/watch?v=F3vdNGFG0LI&list=PLG_pOUNcDW47N8PwD380xECTQtG3hTzJu",
+    "https://www.youtube.com/watch?v=M0mx8S05v60&list=PLBlnK6fEyqRjMH3mWf6kwqiTbT798eAOm",
+    "#",
+    "https://www.youtube.com/watch?v=nBRykKq80KY&list=PLYT7YDstBQmHr7eumHSrdo1aTMpqrpPDa",
+    "https://www.youtube.com/watch?v=aF2uRmibwco&list=PLrjkTql3jnm9U1tSPnPQWQGIGNkUwBFv-",
+    "https://www.youtube.com/watch?v=r59xYe3Vyks&list=PLS1QulWo1RIbfTjQvTdj8Y6yyq4R7g-Al",
+    "https://www.youtube.com/watch?v=wjfeGxqAQOY&list=PLrjkTql3jnm-CLxHftqLgkrZbM8fUt0vn",
+    "https://www.youtube.com/watch?v=58N2N7zJGrQ&list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev",
+    "https://www.youtube.com/watch?v=HOA30z6pIL4&list=PLrk5tgtnMN6TNuhUEf5-UgPxa1wu-l6kP",
+    "#",
+    "https://www.youtube.com/watch?v=s8rsR_TStaA&list=PLBlnK6fEyqRhG6s3jYIU48CqsT5cyiDTO",
+    "https://www.youtube.com/watch?v=0956eoTrbtc&list=PLROvODCYkEM__c0rQvYdenQcCHtY_nreR",
+    "https://www.youtube.com/watch?v=KBulg_u-b3w&list=PLrjkTql3jnm-wW5XdvumCa1u9LjczipjA",
+    "https://www.youtube.com/watch?v=PwFqQfyv0xM&list=PLrjkTql3jnm-iqlEOuVTkmCaRP8F2H3-u",
+    "https://www.youtube.com/watch?v=WxkP5KR_Emk&list=PLrjkTql3jnm9b5nr-ggx7Pt1G4UAHeFlJ",
+    "https://www.youtube.com/watch?v=kmeaG_BQZ7M&list=PLrjkTql3jnm_yol-ZK1QqPSn5YSg0NF9r",
+    "https://www.youtube.com/watch?v=EknEIzswvC0&list=PLS1QulWo1RIbb1cYyzZpLFCKvdYV_yJ-E",
+    "https://www.youtube.com/watch?v=6R7JDkpG1Wk&list=PLrjkTql3jnm8HbdMwBYIMAd3UdstWChFH",
+    "https://www.youtube.com/watch?v=JoeiLuFNBc4&list=PLBlnK6fEyqRgJU3EsOYDTW7m6SUmW6kII",
+    "https://www.youtube.com/watch?v=gmvvaobm7eQ&list=PLeo1K3hjS3uvCeTYTeyfe0-rN5r8zn9rw",
+    "https://www.youtube.com/watch?v=41qgdwd3zAg&list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n",
+    "https://www.youtube.com/watch?v=pg7vElU2dg8&list=PLDYV1XO7FpJXGxGztsM0v8FA0i02RT9pQ",
+    "https://www.youtube.com/watch?v=WxO3Hp9NUVU",
+    "https://www.youtube.com/watch?v=mfePdDh9t6Q&list=PLbMVogVj5nJQJMLb2CYw9rry0d5s0TQRp",
+    "https://www.youtube.com/watch?v=UhgedHMxvjw&list=PL3uLubnzL2TkQ5ZpBIpX34t0cpggGMuIF",
+    "https://www.youtube.com/watch?v=JL_grPUnXzY&list=PLeo1K3hjS3us_ELKYSj_Fth2tIEkdKXvV",
+    "https://www.youtube.com/watch?v=Ic63-yf-zuc&list=PL3uLubnzL2Tm5PAw88N1jR9MLTJpuPEnX",
+    "#"];
+
+const hindiVideoPlaylist = ["https://www.youtube.com/watch?v=XrHtU713vJA&list=PL3qvHcrYGy1v2kJX4SSsurE3_GdVe0ZD5",
+    "#",
+    "https://www.youtube.com/watch?v=uPdeHfcb3t4&list=PLllIzdouGu7BsadiAskySZlhDkfzGldEG",
+    "#",
+    "#",
+    "https://www.youtube.com/watch?v=McOc6OUC7Pc&list=PLU6SqdYcYsfKqa52m3wyMZb1KVWuZsA2T",
+    "https://www.youtube.com/watch?v=xVFMmyROOo4&list=PLzf4HHlsQFwIQUeZq_ykEVB6qZrTRnJZn&index=1",
+    "#",
+    "https://www.youtube.com/watch?v=5UnPqRP236s&list=PLbGui_ZYuhigaBFJWrW6RNnJlhaqQRtK9",
+    "https://www.youtube.com/watch?v=5_5oE5lgrhw&list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi",
+    "https://www.youtube.com/watch?v=c4qlvjVuB4E&list=PL-IC1WV1OE4nNKpgl7t7Vc_wYtBWCZKpl",
+    "https://www.youtube.com/watch?v=R-A57Et24pU&list=PLXOAU6CuWcaGUU3l8JrZhsBs6U1z-YX7s",
+    "https://www.youtube.com/watch?v=Tc_762NIOl0&list=PLbGui_ZYuhigWLoEeaGUHvz1ynoqD6RtZ",
+    "https://www.youtube.com/watch?v=zRnTNBgbgLw&list=PLh38vo46NPOvUXKSmDTYu9uNL5z4CXXGX",
+    "https://www.youtube.com/watch?v=8_KW9lnLY0c",
+    "https://www.youtube.com/watch?v=Tc_762NIOl0&list=PLbGui_ZYuhiilLuOs29220lxrhl3Ee7tO",
+    "#",
+    "https://www.youtube.com/watch?v=u8JZ9gU5o4g&list=PLxCzCOWd7aiHcmS4i14bI0VrMbZTUvlTa",
+    "https://www.youtube.com/watch?v=bkSWJJZNgf8&list=PLxCzCOWd7aiGz9donHRrE9I3Mwn6XdP8p",
+    "https://www.youtube.com/watch?v=VAvsYDtBe78&list=PLbGui_ZYuhij8Oplrvjt_RlDliZQgdxoV",
+    "https://www.youtube.com/watch?v=kBdlM6hNDAE&list=PLxCzCOWd7aiFAN6I8CuViBuCdJgiOkT2Y",
+    "https://www.youtube.com/watch?v=XslI8h7cGDs&list=PLxCzCOWd7aiFM9Lj5G9G_76adtyb4ef7i",
+    "https://www.youtube.com/watch?v=6mbwJ2xhgzM&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
+    "#",
+    "https://www.youtube.com/watch?v=ftKIWPBMWKs&list=PL9RcWoqXmzaIG-RWneeqDJ-FCt66S15pl",
+    "#",
+    "https://www.youtube.com/watch?v=yNZIhF6pxjM&list=PL-JvKqQx2Ate5DWhppx-MUOtGNA4S3spT",
+    "https://www.youtube.com/watch?v=JFF2vJaN0Cw&list=PLxCzCOWd7aiGFBD2-2joCpWOLUrDLvVV_",
+    "https://www.youtube.com/watch?v=uJpQlyT_CK4&list=PLxCzCOWd7aiEed7SKZBnC6ypFDWYLRvB2",
+    "https://www.youtube.com/watch?v=FpvnF7_-tW8&list=PLV8vIYTIdSnYsdt0Dh9KkD9WFEi7nVgbe",
+    "https://www.youtube.com/watch?v=HyU4vkZ2NB8&list=PLjVLYmrlmjGdDps6HAwOOVoAtBPAgIOXL",
+    "https://www.youtube.com/watch?v=vDyHsjHC8h8&list=PL-JvKqQx2Ate6Ii3E0DMtkP9DwAB3NNWY",
+    "https://www.youtube.com/watch?v=9X1rSWLFhLY&list=PL9FuOtXibFjV77w2eyil4Xzp8eooqsPp8",
+    "https://www.youtube.com/watch?v=xS0wLWi5NAs&list=PLuAADu3OvBt7-iBQ4qzW4W2Dn-bu-OWax",
+    "https://www.youtube.com/watch?v=URfNAAuZ52w&list=PLbGui_ZYuhigZkqrHbI_ZkPBrIr5Rsd5L",
+    "https://www.youtube.com/watch?v=TGN48yc9aPU&list=PLhsqmA1RE0NEC15NCay3ObqcGaeHB_ebm",
+    "https://www.youtube.com/watch?v=HhStJ7FDBpc&list=PLYwpaL_SFmcCyQH0n9GHfwviu6KeJ46BV",
+    "https://www.youtube.com/watch?v=n1LkQV10SnE&list=PLR5USSocuZ5f8w9deGEFWqeuq4yNqLzlb",
+    "https://www.youtube.com/watch?v=p6a3cNbabqU&list=PLuAADu3OvBt5W6nsg4WuI7cdqwo_I56UJ",
+    "https://www.youtube.com/watch?v=iipgQQ-4248&list=PLfP3JxW-T70HvifebGl3d5d5jzI1Kp0i8&index=2",
+    "https://www.youtube.com/watch?v=LUWIFzOhkso&list=PLBV6VAQlom0n7iq_d1-uzIt3rBMyXj-kB",
+    "#"];
+
+const teluguVideoPlayliss = ["https://www.youtube.com/watch?v=TrgzcNku0dM&list=PLZ0Bh4GFVGxSIbS-nsbyu0Ch_yaiDaBP_&index=1",
+    "#", "https://www.youtube.com/watch?v=CXr4uTi09fc&list=PLWjHD27kAWxz6tr1lmCjijatMBaAKxqAx&index=1",
+    "https://www.youtube.com/watch?v=scVAW5cCPkA&list=PLa1rpkrS5d_6brR4IdJ19TDxxbFiDv-Yr&index=1", "#",
+    "https://www.youtube.com/watch?v=rcC_dg9Z_ps&list=PLxvz2C0V1lebTL3bCQuo9HlJRhXhBL7l6",
+    "https://www.youtube.com/watch?v=xVFMmyROOo4&list=PLzf4HHlsQFwIQUeZq_ykEVB6qZrTRnJZn&index=1",
+    "https://www.youtube.com/watch?v=G5iY2NSVwW4&list=PLNgoFk5SYUglfSgUdV3OcOXLSGrUtUIhb&index=1",
+    "https://www.youtube.com/watch?v=27UZYywvqb8",
+    "https://www.youtube.com/watch?v=FNCyr9NOhAI&list=PLXiBCNr6xnzqziPq9W-gKHO4PwcMwKwjr",
+    "https://www.youtube.com/watch?v=D13RUiq5tSA&list=PLmFuY5frTUrgATSYYcMZU2o4QvXDXENBz",
+    "https://www.youtube.com/watch?v=ARcxT_0gDeo&list=PLJSrGkRNEDAiSg4o5XRXkcuWSvclNzmtO",
+    "#", "https://www.youtube.com/watch?v=7pKPauQmzOY",
+    "https://www.youtube.com/watch?v=EDbJK1gVjRc&list=PLBwlAgfG_l1dktBeX482c8zuKfAYU-ZdI", "#"];
+
+const teluguVideoPlaylist = ["https://www.youtube.com/watch?v=TrgzcNku0dM&list=PLZ0Bh4GFVGxSIbS-nsbyu0Ch_yaiDaBP_&index=1",
+    "#",
+    "https://www.youtube.com/watch?v=CXr4uTi09fc&list=PLWjHD27kAWxz6tr1lmCjijatMBaAKxqAx&index=1",
+    "https://www.youtube.com/watch?v=scVAW5cCPkA&list=PLa1rpkrS5d_6brR4IdJ19TDxxbFiDv-Yr",
+    "#",
+    "https://www.youtube.com/watch?v=rcC_dg9Z_ps&list=PLxvz2C0V1lebTL3bCQuo9HlJRhXhBL7l6",
+    "https://www.youtube.com/watch?v=xVFMmyROOo4&list=PLzf4HHlsQFwIQUeZq_ykEVB6qZrTRnJZn&index=1",
+    "https://www.youtube.com/watch?v=G5iY2NSVwW4&list=PLNgoFk5SYUglfSgUdV3OcOXLSGrUtUIhb",
+    "https://www.youtube.com/watch?v=DzZzpHQsILE&list=PLqWcha_6aXDvP3cXVqDT7LWVx67M-vO4G",
+    "https://www.youtube.com/watch?v=pm_ugbO2FlY",
+    "https://www.youtube.com/watch?v=FNCyr9NOhAI&list=PLXiBCNr6xnzqziPq9W-gKHO4PwcMwKwjr",
+    "https://www.youtube.com/watch?v=D13RUiq5tSA&list=PLmFuY5frTUrgATSYYcMZU2o4QvXDXENBz",
+    "https://www.youtube.com/watch?v=ARcxT_0gDeo&list=PLJSrGkRNEDAiSg4o5XRXkcuWSvclNzmtO",
+    "#",
+    "https://www.youtube.com/watch?v=f8Xxdhu4baA&list=PLA1HLruLdexSKplfgrgnC5tuEOqrSq3KT",
+    "https://www.youtube.com/watch?v=EDbJK1gVjRc&list=PLBwlAgfG_l1dktBeX482c8zuKfAYU-ZdI",
+    "#",
+    "https://www.youtube.com/watch?v=eNdU3pxIvl4&list=PLBhIctyfOJgDDle-hm8542glhx6vbMWkt",
+    "https://www.youtube.com/watch?v=HOUepU1Ylpw&list=PLJSrGkRNEDAh3p2kJm7LQhmztwGqGQbo6",
+    "https://www.youtube.com/watch?v=7jZBmocOqjw&list=PLv_sM9ZH4RUX80_kQBVolY5te0JWF_V38",
+    "https://www.youtube.com/watch?v=0D30h9SpIHM&list=PL3KKfF5A0sSLnIMTfr7bBw_wRW2vCm3T6",
+    "https://www.youtube.com/watch?v=ayoL_JkeVeY&list=PL6xbXi2C3sePDwyboAcu7l1UYuUT2SWYd",
+    "https://www.youtube.com/watch?v=hSpEEmy7Wo",
+    "#",
+    "#",
+    "https://www.youtube.com/watch?v=Ls-ZGJ5U9fQ&list=PLmAmHQ-_5ySxjnisAfFoOMBJswvKx2FNT",
+    "https://www.youtube.com/watch?v=7VOSHNusQ-k&list=PL6xbXi2C3seOXoXK5nL92Frnk_gOT-_2b",
+    "https://www.youtube.com/watch?v=t3xnhP-gm-Q&list=PLEvM1oTwla97GrH04MOswhIRrPCHoyVwD",
+    "https://www.youtube.com/watch?v=qxv6vPlx2Cs&list=PLmAmHQ-_5ySyCjVtHdSjJ64QU2x5TH8Dy",
+    "https://www.youtube.com/watch?v=atMbjuiPYRg&list=PLv_sM9ZH4RUX3cTixZiSBlCVhG_L9rDzV",
+    "https://www.youtube.com/watch?v=SO_41qUmf2o&list=PLeCpoxUq3EhmgHp7KI-Ih3X2PcNYRtC5y",
+    "https://www.youtube.com/watch?v=aLAFn1rQsSw&list=PL0pCIUQIqHbWhACgZ4Gkg5_KB8Nj8ofL6",
+    "https://www.youtube.com/watch?v=lSAldhEC8Fs&list=PLmAmHQ-_5ySx_dXmOwSuGGGyE8XsbYT0n",
+    "https://www.youtube.com/watch?v=mNB8NlwhAgg&list=PLmAmHQ-_5ySyQeEryrlomrEvOGNYN3TAL",
+    "https://www.youtube.com/watch?v=qn0VbSFeTyY&list=PLLOxZwkBK52DmuHRO3UNpqAzDF57FtIxk",
+    "https://www.youtube.com/watch?v=N2x0h4nX0Qo&list=PLbBdOzdOUWiZa9URsY-FT5QSg40L2hEJR",
+    "https://www.youtube.com/watch?v=xdUk_DU7xNM&list=PL35ft-0sAlPhkcplnefpnc7U4BzLTh_Uh",
+    "#",
+    "https://www.youtube.com/watch?v=YzWQ1dRTWVs&list=PLXOYj6DUOGrrjyRKpD0U0bIKGOXCAOHkE",
+    "https://www.youtube.com/watch?v=WKHlx--15_I",
+    "https://www.youtube.com/watch?v=X2EvPGp3qVE",
+    "#"];
+
+const noteBookLinks = ["https://drive.google.com/file/d/1DyusAJtZNBJeBccOUYY9uB_7lwSF9P6F/view?usp=share_link",
+"https://drive.google.com/file/d/1XeK3loH_TBaKWRpERY1-3tdmcgeEnIDL/view?usp=share_link",
+"https://drive.google.com/file/d/17B5YobEKSNDk6VNSEWPPXoAF3V7DhGxf/view?usp=share_link",
+"https://drive.google.com/file/d/16aZI0q9IG3VWSyeuKgfFhkOGS0t8HT9f/view?usp=share_link",
+"https://drive.google.com/file/d/1cDT6vGdtCoWHfU8JG1oy7l1vk5ZWIWuN/view?usp=share_link",
+"https://drive.google.com/file/d/1wuLcDMLDJ5Y_sII5eolzDcDUXwhFKsF7/view?usp=share_link",
+"https://drive.google.com/file/d/129uLa2XgJW4DC7uwg1-XO93NjTE_wa50/view?usp=share_link",
+"https://drive.google.com/file/d/1JGCGR7SEkU8EibHTuLSswpMY80l2_T7F/view?usp=share_link",
+"https://drive.google.com/file/d/1QhtWrNVMO8n8zh9aO_mxkrjNW2mHQIUb/view?usp=share_link",
+"https://drive.google.com/file/d/14GV8XzzoaJQ5XYRk9iSu6E-GizuKKIsI/view?usp=share_link",
+"https://drive.google.com/file/d/1ABQMm-PNSQk8VRdjjB1v07MOe1sEnCIh/view?usp=share_link",
+"#",
+"https://drive.google.com/file/d/1X9096FJD6l0yjJOogaA8G3T_H8yEtDr9/view?usp=share_link",
+"https://drive.google.com/file/d/1HxwbQ2A564WsC8m5t295RwCyZQrE4Pen/view?usp=share_link",
+"#",
+"https://drive.google.com/file/d/1nX3EFssG_fb8Zz010k2Mrwm3sy2dnMuV/view?usp=share_link",
+"https://drive.google.com/file/d/1iMBTFEXi-w_Xgo09nKkF8D_vgoat29BP/view?usp=share_link",
+"https://drive.google.com/file/d/1r9Rm29VPcAFkvzsSBessy8DiUQKg2prI/view?usp=share_link",
+"https://drive.google.com/file/d/1zEt_5hrrEAjr9otg4v1rpN5mXZFC7lDL/view?usp=share_link",
+"https://drive.google.com/file/d/1kaC5VqxiEn95Bj7y0DgXAMUbg7y1y5V9/view?usp=share_link",
+"https://drive.google.com/file/d/1fFJdom6HmS8zRs2j7sXEMhr3eKihQQ3u/view?usp=share_link",
+"https://drive.google.com/file/d/1OoHLTZ1VxB-ud6hc7wkFGsSsZNVROstq/view?usp=share_link",
+"https://drive.google.com/file/d/13l0BxE0W4fA5OZ3ZjtMcATZP-R2cHY61/view?usp=share_link",
+"https://drive.google.com/file/d/1iCB_8DGD8F2s5JBxZQJgMw9Jc90nBvDi/view?usp=share_link",
+"https://drive.google.com/file/d/1J3hPWeusXRHt-b_FkJhnEl4kkpdH1XpI/view?usp=share_link",
+"https://drive.google.com/file/d/1yna-g8L7AIIep4ioHTiEmBPzRX5XpQRM/view?usp=share_link",
+"https://drive.google.com/file/d/1G8h_nLXCU3_yfK49O2R-cnqfbeRglHWx/view?usp=share_link",
+"https://drive.google.com/file/d/1wraRZ-Cyj8lM5v3LYQvO_asX8QSCxwx8/view?usp=share_link",
+"https://drive.google.com/file/d/1mfMrPUNbfaTUpFE2VaskwLMf1lLzt8fX/view?usp=share_link",
+"https://drive.google.com/file/d/1s4KOx3Bvy73oegS4TmXMtEFbauG-qXRS/view?usp=share_link",
+"https://drive.google.com/file/d/1MbcD2WyNfRsTu5UweslOvwVLj2ngBoRb/view?usp=share_link",
+"https://drive.google.com/file/d/1_AO5CZtq_tlHNP181SL7wmyqZf5R-LFQ/view?usp=share_link",
+"https://drive.google.com/file/d/1FTJ_izYl8WMTDoC04qfKhpZSkXuVHlna/view?usp=share_link",
+"https://drive.google.com/file/d/1CNegeqtFurwyZEEr8Ej9XjNyL9rMo5dg/view?usp=share_link",
+"https://drive.google.com/file/d/18iH80bc-syTe3zksXGfpc8bHrfT10NXi/view?usp=share_link",
+"https://drive.google.com/file/d/1Z3gJcsEDBItoOHQdhBKSkR-TZUmuc4OB/view?usp=share_link",
+"https://drive.google.com/file/d/1uyIW17X2-3X-wsBw5uAPCxzdmGb_MTPO/view?usp=share_link",
+"https://drive.google.com/file/d/1r-CFi0Vbx0pyHWBJdns4zqEn4F036gJ6/view?usp=share_link",
+"#",
+"https://drive.google.com/file/d/1WArYEXH-8uDaT9fKO_ERF7SrXueyCzqn/view?usp=share_link",
+"https://drive.google.com/file/d/1C7KR8Qhq6K8b3cG2qGz7ri4Gk1x1QW5e/view?usp=share_link",
+"https://drive.google.com/file/d/1hcJLV4MMZ6AinyTfVTs-K1R_R4NXUZ_S/view?usp=share_link"];
+
+var videoplaylistlink = "#";
+
+var comwebplaylink = "#";
+
+var comNotelink = "#";
+
+let choosedCourse = localStorage.getItem('subjectcode');
+
+if(choosedCourse == undefined || choosedCourse == null) {
+    alert("Please, Choose the subject.");
+    window.location.assign("category.html");
+}
+
+function webCheck() {
+    comwebplaylink = commonWebsitePlaylist[choosedCourse];
+    if (comwebplaylink == null || comwebplaylink == undefined) {
+        alert("Sorry, currently no Web-Playlist avaliable for the selected subject, will get added soon.(null)");
+    }
+    else if (comwebplaylink == "#") {
+
+        alert("oops!!!,.. currently no Web-Playlist avaliable for the selected subject, will get upadated soon.#Till then try other resources.");
+
+    }
+    else {
+        document.getElementById("playlist1").href = comwebplaylink;
+        document.getElementById("playlist1").target = "_blank";
+    }
+}
+
+function notebookcheck() {
+    comNotelink = noteBookLinks[choosedCourse];
+    if (comNotelink == null || comNotelink == undefined) {
+        alert("Sorry, currently no NoteBook avaliable for the selected subject, will get added soon.(null)");
+    }
+    else if (comNotelink == "#") {
+
+        alert("oops!!!,.. currently no NoteBook avaliable for the selected subject, will get upadated soon.#Till then try other resources.");
+
+    }
+    else {
+        document.getElementById("playlist3").href = comNotelink;
+        document.getElementById("playlist3").target = "_blank";
+    }
+}
+function language(value) {
+
+    
+
+    if (value == "null") {
+        document.getElementById("parachange").innerHTML = "You have choosed to learn " + localStorage.getItem("subject");
+        delete localStorage.language;
+        window.alert("Please, choose the language to get the video playlist.");
+        document.getElementById('playlist2').href = "#";
+        document.getElementById("playlist2").target = "_self";
+
+    }
+    else {
+        localStorage.setItem('language', value);
+        let lan = localStorage.getItem('language');
+
+
+        if (lan == 'English') {
+            videoplaylistlink = englishVideoPlaylist[choosedCourse];
+        }
+
+        else if (lan == 'Hindi') {
+            videoplaylistlink = hindiVideoPlaylist[choosedCourse];
+        }
+
+        else if (lan == 'Telugu') {
+            videoplaylistlink = teluguVideoPlaylist[choosedCourse];
+        }
+
+
+        if (videoplaylistlink == null || videoplaylistlink == undefined) {
+            alert("Sorry, currently no Playlist avaliable for the selected language, will get added soon.(null)");
+        }
+        else if (videoplaylistlink == "#") {
+
+            alert("oops!!!,.. currently no Playlist avaliable for the selected language, will get upadated soon.#Till then try in other language.");
+
+        }
+        else {
+
+            document.getElementById("parachange").innerHTML = "You have choosed to learn " + localStorage.getItem("subject") + " in language " + lan + ".";
+            document.getElementById("playlist2").href = videoplaylistlink;
+            document.getElementById("playlist2").target = "_blank";
+
+        }
+    }
+}
+//delete localStorage.language;
+//delete localStorage.subject;
